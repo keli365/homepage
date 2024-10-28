4383824ab6a239d2f587c5515eff0a14b2e94234
keli365 <keli365@foxmail.com>
2024-10-28 21:07:54 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
       label: '目录',
     },
 
-    /*socialLinks: [{ icon: 'github', link: 'https://github.com/maomao1996/vitepress-nav-template' }],8/
+    /*socialLinks: [{ icon: 'github', link: 'https://github.com/maomao1996/vitepress-nav-template' }],*/
 
     footer: {
       copyright: 'Copyright © Keli365 | Power by maomao',
